421c0aa42fb1d0a5c9791ff571c671215be7acf8
Vivian2810 <vivianmivhel28@gmail.com>
2021-03-15 14:09:34 +0100

    ajout photoloader.js et création directory lib

---
+++ projet-photobox-squelette/projet-photobox-squelette/lib/photoloader.js
@@ -0,0 +1,36 @@
+import {config} from "./config.js";
+
+let photoboxPointEntre = config.photoboxApiRootUri;
+let photoboxServer = config.photoboxHost;
+
+function loadPicture(idPicture){
+    return fetch(photoboxPointEntre+"photos/"+idPicture,{credentials : 'include'})
+        .then(response => {
+            if (response.ok){
+                return response.json();
+            }
+            console.log("response error : "+response.status);
+        })
+        .catch(error => {
+            console.log("Ooops il y a eu une erreur : "+error);
+        });
+}
+
+function loadRessource(uri){
+
+    return fetch(photoboxServer+uri, {
+        credentials: 'include'
+    })
+        .then(response => {
+            if (response.ok)return response.json();
+            console.log("response error : "+response.status);
+        })
+        .catch(error => {
+            console.log("Ooops une erreur s'est produite: "+error);
+        })
+}
+
+export default{
+    loadPicture,
+    loadRessource
+}
