f5a2e0c5ddfa04a4ef6ea2adf4ec39667653904a
Vivian2810 <vivianmivhel28@gmail.com>
2021-03-15 15:38:56 +0100

    fin Exercice 1 question 1/2

--- projet-photobox-squelette/projet-photobox-squelette/lib/ui.js
+++ projet-photobox-squelette/projet-photobox-squelette/lib/ui.js
@@ -2,35 +2,76 @@ import {config} from "./config.js";
 
 let serv = config.photoboxHost;
 
-function displayPicture(photo){
+function displayPicture(photo) {
     //Récupérer la section la_photo
-    let sectionPhoto = document.getElementById("la_photo");
+    let nb = 0;
+    let sectionPhoto = document.getElementById("gallery_container");
     sectionPhoto.innerHTML = `
-        <img src="${serv+photo.photo.url.href}">
-        <h4>${photo.photo.titre}</h4>
-        <p>${photo.photo.descr}</p>
-        <p>${photo.photo.type}, ${photo.photo.width} x ${photo.photo.height}</p>
-        <h4 id="la_categorie">categorie : categorie de l'image</h4>
-        <h4>commentaires : </h4>
-        <ul id="les_commentaires">
-            <li>pseudo : Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi, vitae.</li>
-            <li>pseudo : Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi, recusandae.</li>
-        </ul>
+        <div class="vignette">
+            <img data-uri="/www/canals5/photobox/photos/${photo.photos[0].photo.id}"
+                 src="https://webetu.iutnc.univ-lorraine.fr/www/canals5/photobox/img/small/${photo.photos[0].photo.file}">
+        </div>
+
+        <div class="vignette">
+            <img data-uri="/www/canals5/photobox/photos/${photo.photos[1].photo.id}"
+                 src="https://webetu.iutnc.univ-lorraine.fr/www/canals5/photobox/img/small/${photo.photos[1].photo.file}">
+        </div>
+
+        <div class="vignette">
+            <img data-uri="/www/canals5/photobox/photos/${photo.photos[2].photo.id}"
+                 src="https://webetu.iutnc.univ-lorraine.fr/www/canals5/photobox/img/small/${photo.photos[2].photo.file}">
+        </div>
+
+        <div class="vignette">
+            <img data-uri="/www/canals5/photobox/photos/${photo.photos[3].photo.id}"
+                 src="https://webetu.iutnc.univ-lorraine.fr/www/canals5/photobox/img/small/${photo.photos[3].photo.file}">
+        </div>
+
+        <div class="vignette">
+            <img data-uri="/www/canals5/photobox/photos/${photo.photos[4].photo.id}"
+                 src="https://webetu.iutnc.univ-lorraine.fr/www/canals5/photobox/img/small/${photo.photos[4].photo.file}">
+        </div>
+
+        <div class="vignette">
+            <img data-uri="/www/canals5/photobox/photos/${photo.photos[5].photo.id}"
+                 src="https://webetu.iutnc.univ-lorraine.fr/www/canals5/photobox/img/small/${photo.photos[5].photo.file}">
+        </div>
+
+        <div class="vignette">
+            <img data-uri="/www/canals5/photobox/photos/${photo.photos[6].photo.id}"
+                 src="https://webetu.iutnc.univ-lorraine.fr/www/canals5/photobox/img/small/${photo.photos[6].photo.file}">
+        </div>
+
+        <div class="vignette">
+            <img data-uri="/www/canals5/photobox/photos/${photo.photos[7].photo.id}"
+                 src="https://webetu.iutnc.univ-lorraine.fr/www/canals5/photobox/img/small/${photo.photos[7].photo.file}">
+        </div>
+
+        <div class="vignette">
+            <img data-uri="/www/canals5/photobox/photos/${photo.photos[8].photo.id}"
+                 src="https://webetu.iutnc.univ-lorraine.fr/www/canals5/photobox/img/small/${photo.photos[8].photo.file}">
+        </div>
+        
+        <div class="vignette">
+            <img data-uri="/www/canals5/photobox/photos/${photo.photos[9].photo.id}"
+                 src="https://webetu.iutnc.univ-lorraine.fr/www/canals5/photobox/img/small/${photo.photos[9].photo.file}">
+        </div>
         `;
 }
 
-function displayCategorie(categorie){
+
+function displayCategorie(categorie) {
     let sectionCategorie = document.getElementById("la_categorie");
     sectionCategorie.innerHTML = `categorie : ${categorie.categorie.descr}`;
 }
 
-function displayComments(commentaires){
+function displayComments(commentaires) {
     let sectionComments = document.getElementById("les_commentaires");
-    sectionComments.innerHTML = commentaires.comments.map((commentaire)=>{
+    sectionComments.innerHTML = commentaires.comments.map((commentaire) => {
         return `<li>${commentaire.titre} : ${commentaire.content}</li>`;
-    }).reduce((htmlCode,commentaire)=>{
+    }).reduce((htmlCode, commentaire) => {
         return htmlCode += commentaire;
-    },"");
+    }, "");
 }
 
 export default {
@@ -38,3 +79,10 @@ export default {
     displayCategorie,
     displayComments
 }
+
+/*
+<div class="vignette">
+            <img data-uri="/www/canals5/photobox/photos/"
+                 src="https://webetu.iutnc.univ-lorraine.fr/www/canals5/photobox/img/small/">
+        </div>
+ */
--- projet-photobox-squelette/projet-photobox-squelette/photobox.js
+++ projet-photobox-squelette/projet-photobox-squelette/photobox.js
@@ -1,19 +1,21 @@
 import photoloader from "./lib/photoloader.js";
 import ui from "./lib/ui.js";
+import {config} from "./lib/config.js";
 
 document.querySelector('#load_gallery')
     .addEventListener('click', e => {
-
+        getGalery("/www/canals5/photobox/photos");
     })
 
-const getPicture = function (id) {
+const getGalery = function (uri) {
     // photoloader.loadPicture(id).then(photoDetail => {
     //     console.log("Titre : " + photoDetail.photo.titre);
     //     console.log("Type : " + photoDetail.photo.type);
     //     console.log("URL : " + photoDetail.photo.url.href);
     // })
-    let promesse = photoloader.loadPicture(id);
+    let promesse = photoloader.loadRessource(uri);
+    let promesse2 = photoloader.loadPicture(102);
     promesse.then(ui.displayPicture);
-    promesse.then(getPictureCategorie).then(ui.displayCategorie);
-    promesse.then(getPictureComments).then(ui.displayComments);
+    console.log(promesse);
+    console.log(promesse2);
 }
