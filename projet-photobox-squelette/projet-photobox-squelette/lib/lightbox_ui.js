a933f445b69974376d25e65324bde27446cad9cf
Vivian2810 <vivianmivhel28@gmail.com>
2021-03-22 14:16:27 +0100

    avancement exo 2 modif css et lightbox_ui.js

--- projet-photobox-squelette/projet-photobox-squelette/lib/lightbox_ui.js
+++ projet-photobox-squelette/projet-photobox-squelette/lib/lightbox_ui.js
@@ -1,5 +1,15 @@
 import lightbox from "./lightbox.js";
 
+var container = document.getElementById("lightbox_container");
+
 function display_lightbox(data){
 
+}
+
+function show(){
+    container.className = "lightbox_container--hidden";
+}
+
+function hide(){
+    container.className = "lightbox_container--visible";
 }
